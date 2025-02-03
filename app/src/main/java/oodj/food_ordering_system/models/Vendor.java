82ca00dd0db3f78cbf68205de08db3dda1d4ef3f
CHUA YI HONG <111723846+darylcyh@users.noreply.github.com>
2025-02-03 10:59:08 +0800

    N/a

--- app/src/main/java/oodj/food_ordering_system/models/Vendor.java
+++ app/src/main/java/oodj/food_ordering_system/models/Vendor.java
@@ -1,15 +1,60 @@
 package oodj.food_ordering_system.models;
 
 public class Vendor {
+    // Attributes
+    private int vendorID;
+    private String vendorName;
+    private String foodCourtName;
+    private String contactNumber;
+    private String username;
+    private String password;
+    private boolean status; // true for active, false for inactive
+    private String dateRegistered;
 
-    private String username, password, name, address, contactNo;
-
-    public Vendor(String username, String password, String name, String address, String contactNo) {
+    // Constructor
+    public Vendor(int vendorID, String vendorName, String foodCourtName, String contactNumber, 
+                  String username, String password, boolean status, String dateRegistered) {
+        this.vendorID = vendorID;
+        this.vendorName = vendorName;
+        this.foodCourtName = foodCourtName;
+        this.contactNumber = contactNumber;
         this.username = username;
         this.password = password;
-        this.name = name;
-        this.address = address;
-        this.contactNo = contactNo;
+        this.status = status;
+        this.dateRegistered = dateRegistered;
+    }
+
+    // Getters and Setters
+    public int getVendorID() {
+        return vendorID;
+    }
+
+    public void setVendorID(int vendorID) {
+        this.vendorID = vendorID;
+    }
+
+    public String getVendorName() {
+        return vendorName;
+    }
+
+    public void setVendorName(String vendorName) {
+        this.vendorName = vendorName;
+    }
+
+    public String getFoodCourtName() {
+        return foodCourtName;
+    }
+
+    public void setFoodCourtName(String foodCourtName) {
+        this.foodCourtName = foodCourtName;
+    }
+
+    public String getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(String contactNumber) {
+        this.contactNumber = contactNumber;
     }
 
     public String getUsername() {
@@ -19,7 +64,7 @@ public class Vendor {
     public void setUsername(String username) {
         this.username = username;
     }
-    
+
     public String getPassword() {
         return password;
     }
@@ -27,34 +72,38 @@ public class Vendor {
     public void setPassword(String password) {
         this.password = password;
     }
-    
-    public String getName() {
-        return name;
+
+    public boolean isStatus() {
+        return status;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-    
-    public String getAddress() {
-        return address;
+    public void setStatus(boolean status) {
+        this.status = status;
     }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
-    
-    public String getContactNo() {
-        return contactNo;
+    public String getDateRegistered() {
+        return dateRegistered;
     }
 
-    public void setContactNo(String contactNo) {
-        this.contactNo = contactNo;
+    public void setDateRegistered(String dateRegistered) {
+        this.dateRegistered = dateRegistered;
     }
-    
-    @Override
-    public String toString() {
-        return "Vendor{" + "username=" + username + ", password=" + password + ", name=" + name + ", address=" + address + ", contactNo=" + contactNo + '}';
+
+    // Methods
+    public void activateVendor() {
+        this.status = true;
     }
-    
+
+    public void deactivateVendor() {
+        this.status = false;
+    }
+
+    public String displayVendorInfo() {
+        return "VendorID: " + vendorID + 
+               "\nVendorName: " + vendorName + 
+               "\nFoodCourtName: " + foodCourtName + 
+               "\nContactNumber: " + contactNumber + 
+               "\nStatus: " + (status ? "Active" : "Inactive") +
+               "\nDateRegistered: " + dateRegistered;
+    }
 }
