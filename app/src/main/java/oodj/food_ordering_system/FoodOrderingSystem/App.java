d5c9f0973ef275529150dcfb3b49193c86c1b124
CHUA YI HONG <111723846+darylcyh@users.noreply.github.com>
2025-02-18 03:36:42 +0800

    rating columm

---
+++ app/src/main/java/oodj/food_ordering_system/FoodOrderingSystem/App.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oodj.food_ordering_system.FoodOrderingSystem;
+
+/**
+ *
+ * @author daryl
+ */
+public class App {
+    public static void main(String[] args) {
+        CarRentalSystem.main(args);
+    }
+}
